ae3766dec3eaa41eee93fbdde0eef13f86b5ce9c
Tibieriu <tibipaiu97@gmail.com>
2021-12-18 20:05:33 +0100

    add the path highestBidOffers/add/:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
   { path: 'bids', component: BidListComponent, canActivate: [LoggedInGuard]},
   { path: 'bids/make/:id', component: BidMakeComponent, canActivate: [LoggedInGuard]},
 
-  { path: 'highestBidOffers/add', component: HighestBidOfferAddComponent, canActivate: [LoggedInGuard]},
+  { path: 'highestBidOffers/add/:id', component: HighestBidOfferAddComponent, canActivate: [LoggedInGuard]},
   { path: 'highestBidOffers', component: HighestBidOfferListComponent, canActivate: [LoggedInGuard]},
   { path: 'highestBidOffers/:id', component: HighestBidOfferDetailComponent, canActivate: [LoggedInGuard]},
   { path: 'highestBidOffers/:id/bid', component: BidMakeComponent , canActivate: [LoggedInGuard]},
