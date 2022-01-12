08de5d3771aefa7697b1e0687aaecca2ba9f9846
Sanel Novalić <sanel.novalic@edu.fit.ba>
2022-01-12 21:27:20 +0100

    Display highest bid offers

--- src/app/nft/nft-details/nft-detail.component.ts
+++ src/app/nft/nft-details/nft-detail.component.ts
@@ -9,6 +9,8 @@ import { AuthenticationBasicService } from '../../login-basic/authentication-bas
 import {NgbModal, NgbModalConfig} from '@ng-bootstrap/ng-bootstrap';
 import { User } from '../../login-basic/user';
 import {Subject} from 'rxjs';
+import {HighestBidOfferService} from "../../highestBidOffer/highestBidOffer.service";
+import {HighestBidOffer} from "../../login-basic/highestBidOffer";
 @Component({
   selector: 'app-nft-detail',
   templateUrl: './nft-detail.component.html',
@@ -20,11 +22,13 @@ export class NftDetailComponent implements OnInit {
   public user: User = new User();
   public users: User[] = [];
   public offers: Offer[] = [];
+  public highestBidOffers: HighestBidOffer[] = [];
   public pageSize = 5;
   public page = 1;
   public totalOffers = 0;
   public totalUsers = 0;
   public totalFavorites = 0;
+  public totalHighestBidOffer = 0;
   private success = new Subject<string>();
   successMessage = '';
   constructor(private route: ActivatedRoute,
@@ -32,6 +36,7 @@ export class NftDetailComponent implements OnInit {
               private nftService: NftService,
               private userService: UserService,
               private offerService: OfferService,
+              private highestBidOfferService: HighestBidOfferService,
               private authenticationService: AuthenticationBasicService,
               config: NgbModalConfig, private modalService: NgbModal) {
   }
@@ -57,24 +62,24 @@ export class NftDetailComponent implements OnInit {
     // Get all the offers on this nft
     this.offerService.getAll({size: this.pageSize}).subscribe(
       (offers: Offer[]) => {
-        const offersAll = [];
         this.offers = offers;
         this.totalOffers = this.offerService.totalElement();
         for (const offer of this.offers) {
           offer.getRelation(NFT, 'nft').subscribe((nft: any) => {
             offer.nft = nft.uri;
-            console.log(offer.nft);
             if (offer.nft === '/nFTs/' + id){
-              offersAll.push(offer);
+              if (offer.uri.split('/')[1] === 'highestBidOffers') {
+                this.highestBidOfferService.get(offer.uri.split('/')[2]).subscribe((highestBidOffer: HighestBidOffer) => {
+                  this.highestBidOffers.push(highestBidOffer);
+                });
+              }
             }
           });
         }
-        this.totalOffers = offersAll.length;
         this.offers = offersAll;
-        console.log(this.totalOffers);
+        this.totalOffers = offersAll.length;
+        console.log(this.offers[0]);
       });
-
-
     // How many favorites does an NFT have
     this.userService.getAll({size: this.pageSize}).subscribe(
       (users: User[]) => {
