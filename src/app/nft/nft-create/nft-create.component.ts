ffae08d27ad4b7b06e49f7730a9d6dd63a08e220
Tibieriu <tibipaiu97@gmail.com>
2022-01-19 17:55:17 +0100

    when creating an ntf that is assigned the user who created it

--- src/app/nft/nft-create/nft-create.component.ts
+++ src/app/nft/nft-create/nft-create.component.ts
@@ -4,6 +4,8 @@ import {Location } from '@angular/common';
 
 import {NFT} from '../../login-basic/nft';
 import {NftService } from '../nft.service';
+import {User} from '../../login-basic/user';
+import {AuthenticationBasicService} from '../../login-basic/authentication-basic.service';
 
 @Component({
   selector: 'app-nft-create',
@@ -12,15 +14,18 @@ import {NftService } from '../nft.service';
 export class NftCreateComponent implements OnInit {
   public nft: NFT;
   public idNFT: string;
+  public owner: User;
 
   constructor(private route: Router,
               private location: Location,
               private nftService: NftService,
+              private authenticationService: AuthenticationBasicService
   ) {
   }
 
   ngOnInit(): void {
     this.idNFT = this.route.url.substr(0, this.route.url.lastIndexOf('/nFTs'));
+    this.owner = this.authenticationService.getCurrentUser();
     this.nft = new NFT();
   }
 
